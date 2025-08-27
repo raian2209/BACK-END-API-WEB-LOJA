b01e6d230a0e8acf4683d637db0d11ffabe159f1
Jonas Sales <jonassales510@gmail.com>
2025-08-27 19:27:28 -0300

    update: Adding GET, PUT and DELETE methods.

--- sistema-loja/src/main/java/br/com/suaempresa/apigerenciamento/coupon/controller/CupomController.java
+++ sistema-loja/src/main/java/br/com/suaempresa/apigerenciamento/coupon/controller/CupomController.java
@@ -9,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/cupons")
 @PreAuthorize("hasRole('ADMIN')") // Protege todos os métodos da classe
@@ -27,8 +29,29 @@ public class CupomController {
     }
 
     // TODO GET
+    @GetMapping
+    public ResponseEntity<List<CupomResponseDTO>> findAllCupom() {
+        return ResponseEntity.ok(cupomService.listCupom());
+    }
+
+    //TODO GET BY ID
+    @GetMapping("/{id}")
+    public ResponseEntity<CupomResponseDTO> findCupomById(@PathVariable Long id) {
+        return ResponseEntity.ok(cupomService.findCupom(id));
+    }
 
     // TODO PUT
+    @PutMapping("/{id}")
+    public ResponseEntity<CupomResponseDTO> updateCupom(@PathVariable("id") Long id, @Valid @RequestBody CupomRequestDTO requestDTO) {
+        CupomResponseDTO responseDTO = cupomService.updateCupom(id, requestDTO);
+        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
+    }
+
 
     // TODO DELETE
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCupom(@PathVariable Long id) {
+        cupomService.deleteCupom(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- sistema-loja/src/main/java/br/com/suaempresa/apigerenciamento/coupon/service/CupomService.java
+++ sistema-loja/src/main/java/br/com/suaempresa/apigerenciamento/coupon/service/CupomService.java
@@ -3,9 +3,13 @@ import br.com.suaempresa.apigerenciamento.coupon.dto.CupomRequestDTO;
 import br.com.suaempresa.apigerenciamento.coupon.dto.CupomResponseDTO;
 import br.com.suaempresa.apigerenciamento.coupon.model.Cupom;
 import br.com.suaempresa.apigerenciamento.coupon.repository.CupomRepository;
+import br.com.suaempresa.apigerenciamento.exception.CupomNotFoundException;
+import br.com.suaempresa.apigerenciamento.exception.ProdutoNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CupomService {
@@ -31,17 +35,58 @@ public class CupomService {
     }
 
     // TODO findAll
-
+    @Transactional(readOnly = true)
+    public List<CupomResponseDTO> listCupom() {
+        return cupomRepository.findAll()
+                .stream()
+                .map(this::mapToResponseDTO)
+                .collect(Collectors.toList());
+    }
 
     // TODO findById
+    @Transactional(readOnly = true)
+    public CupomResponseDTO findCupom(Long id) {
+        Cupom cupom = cupomRepository.findById(id)
+                .orElseThrow(() -> new ProdutoNotFoundException("Produto não encontrado com o ID: " + id));
+        return mapToResponseDTO(cupom);
+    }
+
 
     // TODO update
+    @Transactional
+    public CupomResponseDTO updateCupom(Long id,CupomRequestDTO requestDTO) {
+        Cupom cupom = cupomRepository.findById(id)
+                .orElseThrow(() -> new CupomNotFoundException("Cupom não encontrado com o ID: " + id));
 
+        cupom.setTipoDesconto(requestDTO.getTipoDesconto());
+        cupom.setValorDesconto(requestDTO.getValorDesconto());
+        cupom.setDataValidade(requestDTO.getDataValidade());
+        cupom.setAtivo(cupom.isAtivo());
+
+        Cupom updateCupom = cupomRepository.save(cupom);
+
+        return mapToResponseDTO(updateCupom);
+    }
 
     // TODO delete
+    @Transactional
+    public void deleteCupom(Long id) {
+
+        cupomRepository.findById(id)
+                .orElseThrow(() -> new ProdutoNotFoundException("Produto não encontrado com o ID: " + id));
+
+        // A anotação @SQLDelete no model fará a mágica do Soft Delete
+        cupomRepository.deleteById(id);
+    }
 
     private CupomResponseDTO mapToResponseDTO(Cupom cupom) {
-        // Lógica de mapeamento...
-        return new CupomResponseDTO(); // Implementação omitida por brevidade
+        CupomResponseDTO dto = new CupomResponseDTO();
+        dto.setId(cupom.getId());
+        dto.setCodigo(cupom.getCodigo().toUpperCase());
+        dto.setTipoDesconto(cupom.getTipoDesconto());
+        dto.setValorDesconto(cupom.getValorDesconto());
+        dto.setDataValidade(cupom.getDataValidade());
+        dto.setAtivo(cupom.isAtivo());
+        return  dto;
     }
 }
