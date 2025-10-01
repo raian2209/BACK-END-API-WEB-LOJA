c89e48f5c8ec4724712fbacc36e453296719e54b
Jonas Sales <jonassales510@gmail.com>
2025-10-01 16:23:27 -0300

    update: add @NotNull annotation.

--- sistema-loja/src/main/java/br/com/suaempresa/apigerenciamento/product/dto/ProdutoRequestDTO.java
+++ sistema-loja/src/main/java/br/com/suaempresa/apigerenciamento/product/dto/ProdutoRequestDTO.java
@@ -1,6 +1,7 @@
 package br.com.suaempresa.apigerenciamento.product.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,6 +15,7 @@ public class ProdutoRequestDTO {
 
     private String descricao;
 
+    @NotNull(message = "se deve ter um valor")
     @Positive(message = "O preço deve ser um valor positivo.")
     private Double preco;
 }
