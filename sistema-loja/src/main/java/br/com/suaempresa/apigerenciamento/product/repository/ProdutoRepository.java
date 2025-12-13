80c5e2f1e06d7353feb2571755a4d057a481f34b
Jonas Sales <jonassales510@gmail.com>
2025-12-13 09:49:18 -0300

    update: add query to return products by Fornecedor and total of products by Fornecedor.

--- sistema-loja/src/main/java/br/com/suaempresa/apigerenciamento/product/repository/ProdutoRepository.java
+++ sistema-loja/src/main/java/br/com/suaempresa/apigerenciamento/product/repository/ProdutoRepository.java
@@ -4,6 +4,11 @@ import br.com.suaempresa.apigerenciamento.product.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+    long countByFornecedorId(Long fornecedorId);
+
+    List<Produto> findByFornecedorId(Long fornecedorId);
 }
